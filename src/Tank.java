ff8ec47b8715df5f43b226bdf306b229d3519dbb
MilkeySummer <andrey222930@gmail.com>
2025-11-06 01:18:06 +0300

    all done

--- src/Tank.java
+++ src/Tank.java
@@ -8,7 +8,7 @@ public class Tank {
     public void newGame(){
         currentPlayer = "X";
         gameEnded = false;
-
+s1
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 field[i][j] = "-";
